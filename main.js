804d91485e7f86363d2b5d50fcf5d065ee3a9d94
sabbir <mdsabbirrahmanm740@gmail.com>
2023-07-23 17:08:33 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const btn=document.querySelector('#btn');
+const show=document.querySelector('#show');
+btn.addEventListener('click',function(){
+show.classList.toggle('hidden')
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    fontFamily:{
+      roboto:['Roboto', 'sans-serif'],
+      opensans:['Open Sans', 'sans-ser']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
